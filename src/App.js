34a90084a4ab7c72c2eac5fa97c0f86a6dd665e9
Matthew Rhoads <mematthew123@gmail.com>
2022-05-11 17:56:44 -0600

    built the stats coomponent,

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import { useState } from 'react'
+import FeedbackStats from './components/FeedbackStats'
 
 function App() {
 
@@ -24,6 +25,8 @@ function App() {
     <>
       <Header />
       <div className="container">
+
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback}
         
         handleDelete={deleteFeedback}
